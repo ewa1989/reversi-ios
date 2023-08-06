322d11220d7cbdd8b9f30685ce6e41d29d8ba1ab
Yusuke Uchida <65172567+ewa1989@users.noreply.github.com>
2023-08-06 22:22:33 +0900

    ゲーム開始後1手置くとパスが発生するテストを追加

--- ReversiTests/Presentation/ViewModelTest.swift
+++ ReversiTests/Presentation/ViewModelTest.swift
@@ -36,7 +36,7 @@ final class ViewModelTest: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    // MARK: ゲーム読み込み
+    // MARK: ゲーム読み込みが保存ゲームの有無で正しく分岐する
 
     func test_保存されたゲームがない時_画面ロード時に新ゲームが生成され_保存もされる() throws {
         let expected = ReversiGame.newGame()
@@ -60,7 +60,7 @@ final class ViewModelTest: XCTestCase {
         XCTAssertEqual(try viewModel.game.toBlocking().first(), expected)
     }
 
-    // MARK: コンピューターの試行
+    // MARK: コンピューターの試行でセルの描画が正しく行われる
 
     func test_viewDidAppearが呼ばれるとゲームが開始され_コンピューターが思考しディスクを置き_セル2つの描画が更新され_描画が完了するとゲームが保存される() throws {
         fakeStrategy.fakeInput = TestData.startFromDarkComputerOnlyPlaceAt2_0.rawValue
@@ -120,7 +120,7 @@ final class ViewModelTest: XCTestCase {
         XCTAssertEqual(fakeStrategy.fakeOutput, "o10\nxxx---xo\n--------\n--------\n--------\n--------\n--------\n--------\n--------\n")
     }
 
-    // MARK: ユーザーの試行
+    // MARK: ユーザーの試行でセルの描画が正しく行われる
 
     func test_白から始まる新規ゲームで_ユーザーが4_2にディスクを置くと_セル2つの描画が更新され_描画が完了するとゲームが保存される() throws {
         fakeStrategy.fakeInput = TestData.newGameStartFromLight.rawValue
@@ -228,4 +228,49 @@ final class ViewModelTest: XCTestCase {
         // 保存処理は走らない
         XCTAssertEqual(fakeStrategy.fakeOutput, nil)
     }
+
+    // MARK: パスの判定が正しくされ了承でターンが変わる
+
+    func test_ディスクが置かれターンが変わった時_置く場所がないとアラートが表示され_了承するとターンが変わる() throws {
+        fakeStrategy.fakeInput = TestData.mustPassOnNextTurn.rawValue
+
+        let message = viewModel.message.makeTestableObserver(testScheduler: scheduler, disposeBag: disposeBag)
+        let passAlert = viewModel.passAlert.makeTestableObserver(testScheduler: scheduler, disposeBag: disposeBag)
+
+        // 描画完了を通知するタイミングはアサーション上重要ではないのでコントロールせず、即描画完了とする
+        viewModel.diskToPlace.subscribe{ [weak self] _ in
+            self?.viewModel.finishToPlace(isFinished: true)
+        }.disposed(by: disposeBag)
+
+        scheduler.createColdObservable([
+            .next(1, (1)),
+            .next(2, (2)),
+            .next(3, (3)),
+            .next(4, (4)),
+        ]).subscribe { [weak self] event in
+            switch event.element {
+            case 1:
+                self?.viewModel.viewDidLoad()   // ViewControllerが読み込まれ
+            case 2:
+                self?.viewModel.viewDidAppear() // ViewControllerが表示され
+            case 3:
+                self?.viewModel.didSelectCellAt(x: 2, y: 0) // (2, 0)に黒を置くと白はどこにも置けなくなる
+            default:
+                self?.viewModel.pass()  // パスを了承すると黒のターンに変わる
+            }
+        }.disposed(by: disposeBag)
+        scheduler.start()
+
+        XCTAssertEqual(message.events, [
+            .next(0, Message(disk: nil, label: "Tied")),        // 初期状態
+            .next(1, Message(disk: .dark, label: "'s turn")),   // ゲーム読み込み後
+            .next(3, Message(disk: .light, label: "'s turn")),  // パスするアラートが表示されてる最中は白のターンと表示される
+            .next(4, Message(disk: .dark, label: "'s turn")),   // パス後黒にターンが移る
+        ])
+        XCTAssertEqual(passAlert.events, [.next(3, PassAlert())])
+        // 保存されるのは黒がディスクを置いて描画完了したところまで
+        XCTAssertEqual(fakeStrategy.fakeOutput, "o00\nxxx-----\no-------\n--------\n--------\n--------\n--------\n--------\n--------\n")
+    }
+
+    // TODO: ゲーム読み込み時からパスが必要なケースは現状バグがある。他のテストを書き終わった後にテストを作成し、バグ修正する
 }
